5fcd9cbb8f86c5fea2cf756e7c385441f0a5e94d
Eric Mugo <mugoericpg20@gmail.com>
2023-05-22 11:34:43 +0300

    SLASHED CUSTOMER NAME

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,18 @@
-# Write your code here.
+katz_deli = []
+
+
+
+def line(katz_deli)
+    if katz_deli.empty?
+        puts "The line is currently empty."
+    else
+        line_status = "The line is currently:"
+        katz_deli.each_with_index do |customer, index|
+            line_status += " #{index + 1}. #{customer}"
+        end
+        puts line_status
+    end
+end
+
+def take_a_number(kats_deli, customer_name)
+    kats_deli << customer_name
